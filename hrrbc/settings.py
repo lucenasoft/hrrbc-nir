a9e25d47c5a03a9a1bc33fd60aaed6ca38ed8c85
lucenasoft <getugbr@gmail.com>
2023-01-01 14:24:03 -0300

    feat: update in settings for disable debug opt

--- hrrbc/settings.py
+++ hrrbc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#on=xlkdbqa9rpwqk*71%7zz)(yk@ub!_q82&+!4@ewryka+du'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.140.100']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
